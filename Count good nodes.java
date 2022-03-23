78a5b282d00aac67634a0870e788dfe5d9b4b5fe
Sowndar K <84360457+KSOWNDAR@users.noreply.github.com>
2022-03-23 12:11:56 +0530

    Create Count good nodes.java

---
+++ Count good nodes.java
@@ -0,0 +1,77 @@
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+
+Return the number of good nodes in the binary tree.
+
+ 
+
+Example 1:
+
+
+
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+Root Node (3) is always a good node.
+Node 4 -> (3,4) is the maximum value in the path starting from the root.
+Node 5 -> (3,4,5) is the maximum value in the path
+Node 3 -> (3,1,3) is the maximum value in the path.
+Example 2:
+
+
+
+Input: root = [3,3,null,4,2]
+Output: 3
+Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+Example 3:
+
+Input: root = [1]
+Output: 1
+Explanation: Root is considered as good
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count=1;
+    public int goodNodes(TreeNode root) {
+        if(root == null)
+            return 0;
+        if(root.left!=null)
+        {
+        if(root.left.val>=root.val)
+        {
+            count++;
+        }
+        else
+        {
+            root.left.val=root.val;
+        }
+        }
+        if(root.right!=null)
+        {
+        if(root.right.val>=root.val)
+        {
+            count++;
+        }
+        else
+        {
+            root.right.val=root.val;
+        }
+        }
+        goodNodes(root.left);
+        goodNodes(root.right);
+        return count;
+    }
+}
