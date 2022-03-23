4c53661650e9f293c0241bd651e1b37a7c6c8b1b
Sowndar K <84360457+KSOWNDAR@users.noreply.github.com>
2022-03-23 10:21:06 +0530

    Create Construct Binary tree from pre and inorder.java

---
+++ Construct Binary tree from pre and inorder.java
@@ -0,0 +1,56 @@
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+ 
+
+Example 1:
+
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer,Integer> inMap=new HashMap<>();
+        
+        for(int i=0;i<inorder.length;i++)
+        {
+            inMap.put(inorder[i],i);
+        }
+        
+        TreeNode root=buildTree(preorder,0,preorder.length-1,inorder,0,inorder.length-1,inMap);
+        return root;
+    }
+    public TreeNode buildTree(int[] preorder,int preStart,int preEnd,int[] inorder,int inStart,int inEnd,Map<Integer,Integer> inMap)
+    {
+        if(preStart>preEnd || inStart>inEnd)
+            return null;
+        TreeNode root=new TreeNode(preorder[preStart]);
+        
+        int inRoot=inMap.get(root.val);
+        int numsLeft=inRoot-inStart;
+        
+        root.left=buildTree(preorder,preStart+1,preStart+numsLeft,inorder,inStart,inRoot-1,inMap);
+        root.right=buildTree(preorder,preStart+numsLeft+1,preEnd,inorder,inRoot+1,inEnd,inMap);
+        
+        return root;
+    }
+}
